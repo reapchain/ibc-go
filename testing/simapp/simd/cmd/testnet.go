ce80f22a871f48a54ef8ece374deeb58d9c3720e
comnic <comnics@gmail.com>
2022-05-03 09:57:38 +0900

    Modified Testing.

--- testing/simapp/simd/cmd/testnet.go
+++ testing/simapp/simd/cmd/testnet.go
@@ -213,6 +213,7 @@ func InitTestnet(
 			stakingtypes.NewDescription(nodeDirName, "", "", "", ""),
 			stakingtypes.NewCommissionRates(sdk.OneDec(), sdk.OneDec(), sdk.OneDec()),
 			sdk.OneInt(),
+			"standing",
 		)
 		if err != nil {
 			return err
